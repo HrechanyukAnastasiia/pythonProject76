c6155be1dca708276015f497333284e0c2489d1c
Hrechanyuk Anastasiia <anastasiya.grk@gmail.com>
2023-05-08 21:37:18 +0300

    №2

---
+++ 14.05.py
@@ -0,0 +1,10 @@
+#2
+import math
+class Circle:
+    def __init__(self, radius):
+        self.radius = radius
+    def area(self):
+        return math.pi * (self.radius ** 2)
+name = Circle(10)
+print(f"Радіус кола: {name.radius}")
+print(f"Площа кола: {round(name.area(), 2)} см^2")
